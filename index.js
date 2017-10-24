11374a55d1bdb0157ec6a9c8a2d0f5ded9e59c5f
A Q Khan <aqk.neo@gmail.om>
2017-10-24 22:29:03 +0500

    Rewired index.js

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
+// Import libraries to create a component
 
-AppRegistry.registerComponent('albums', () => App);
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import Header from './src/components/Header';
+
+// Create a component
+
+const Test = () => {
+    return(
+        <Header headerText='Albums' />
+    );
+};
+
+// Render the component to device
+
+AppRegistry.registerComponent('albums', () => Test);
